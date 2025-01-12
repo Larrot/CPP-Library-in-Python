c2fcfd4a5ef6719abae5fd6b6af3afc66366c073
Larrot <93649065+Larrot@users.noreply.github.com>
2025-01-12 19:30:25 +0500

    Delete The simplest case  directory

--- The simplest case /example.cpp
+++
@@ -1,9 +0,0 @@
-// example.cpp
-#include <iostream>
-
-extern "C" {
-    void my_function() {
-        std::cout << "Hello from C++" << std::endl;
-    }
-}
-
--- The simplest case /lib.py
+++
@@ -1,7 +0,0 @@
-import ctypes
-
-# Load the shared library
-lib = ctypes.CDLL('./libexample.so')  # or 'example.dll' on Windows
-
-# Call the function
-lib.my_function()
